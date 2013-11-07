fd943ed6ef1d967c73b5a9931ff1e61628c05279
Federico Pooleye <federicopooley@hotmail.com>
2013-11-07 17:05:49 -0300

    Associated users and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
+  #update the model. Add belong_to.
   attr_accessible :body, :title
+  default_scope order('created_at DESC')
 end
