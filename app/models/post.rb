15f55386c19a74dcf26b46ebd6fddf3fc7d3c390
Federico Pooleye <federicopooley@hotmail.com>
2013-11-10 19:00:23 -0300

    Associated and nested topics and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  belongs_to :topic
   #update the model. Add belong_to.
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :topic
   default_scope order('created_at DESC')
 end
